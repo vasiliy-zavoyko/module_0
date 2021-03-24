39f3aea83246a0660bc4875e7acde44aa1ec233b
Vasiliy Zavoyko <vasiliy.zavoyko@gmail.com>
2021-03-24 16:52:12 +0300

    cleanup

--- game.py
+++ game.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def game_core_v1(number):
-    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
-       Функция принимает загаданное число и возвращает число попыток'''
+    """Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток"""
     count = 0
     while True:
         count += 1
@@ -13,8 +13,8 @@ def game_core_v1(number):
 
 
 def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
+    """Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток"""
     count = 1
     predict = np.random.randint(1, 101)
     while number != predict:
@@ -23,20 +23,20 @@ def game_core_v2(number):
             predict += 1
         elif number < predict:
             predict -= 1
-    return (count)  # выход из цикла, если угадали
+    return count  # выход из цикла, если угадали
 
 
 def game_core_v3(number):
+    """Извращения над бинарным поиском"""
     count = 1
 
-    # Извращения над бинарным поиском
     def inner(start, end):
         mid = (start + end) // 2
-        if (mid == number):
+        if mid == number:
             return
         nonlocal count
         count += 1
-        if (mid > number):
+        if mid > number:
             inner(start, mid - 1)
         else:
             inner(mid + 1, end)
@@ -46,15 +46,15 @@ def game_core_v3(number):
 
 
 def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(1000))
+    random_array = np.random.randint(1, 101, size=1000)
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return (score)
+    return score
 
 
 score_game(game_core_v1)
